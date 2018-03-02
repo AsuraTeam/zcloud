6f01cf2849f790d7a917f4229fb740cc2748c314
Your Name <you@example.com>
2018-03-02 13:09:30 +0800

    删除无效

--- 1.sh
+++ 1.sh
@@ -3,7 +3,7 @@ echo "1.sh 描述信息"
 exit
 fi
 svn up
-rsync -avr ../cloud/ ../zcloud/ --exclude=key/* --exclude=conf/*  --exclude=.svn --exclude=.git --delete --exclude=zcloud.iml --exclude=make.go
+rsync -avr ../cloud/ ../zcloud/ --exclude=key/* --exclude=conf/*  --exclude=.svn --exclude=.git --delete --exclude=zcloud.iml --exclude=make.go --exclude=.idea
 cd ../zcloud/
 git add .
 git commit -m $1
