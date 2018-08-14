902a1f609ee7bd43fb687b6259265e552f112757
zhaozhiqiang <zhaozhiqiang@chehejia.com>
2018-08-14 12:27:06 +0800

    容器状态刷新

--- cloud.sql
+++ cloud.sql
@@ -1447,6 +1447,7 @@ CREATE TABLE `cloud_registry_server` (
   `mount_path` varchar(1000) DEFAULT NULL COMMENT '仓库挂载路径',
   `labels` varchar(30) DEFAULT NULL COMMENT '安装集群的标签',
   `host_path` varchar(200) ) DEFAULT NULL COMMENT '主机挂载路径',
+  `status` varchar(10) DEFAULT NULL COMMENT '运行状态',
   `replicas` int(11) DEFAULT NULL COMMENT '副本数量',
   PRIMARY KEY (`server_id`),
   UNIQUE KEY `uidx_cloud_registry_server_clsuter_name` (`cluster_name`,`name`),
